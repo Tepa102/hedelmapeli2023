9f5071d2d842fdd5e70c50c9d718350e4ef54362
Terttu Toivonen <terttu.toivonen@esedulainen.fi>
2023-10-27 15:48:08 +0300

    rullien lukituksen rajoittaminen 3 rullaan

--- peli.js
+++ peli.js
@@ -21,6 +21,7 @@ let saldo = 100;
 let valittuPanos = 1;
 let lukitutRullat = [false, false, false, false];
 
+
 const rullaElementit = document.querySelectorAll('.rulla');
 const saldoElementti = document.getElementById('saldo');
 const panosElementti = document.getElementById('panos');
@@ -44,9 +45,12 @@ function pyoraytaRullat() {
     for (let i = 0; i < rullaElementit.length; i++) {
         if (!lukitutRullat[i]) {
             const uusiKuva = arvoKuva();
+            console.log(`Rulla ${i} pyörähti ja näyttää kuvan: ${uusiKuva}`);
             rullaElementit[i].textContent = uusiKuva;
         }
     }
+    // Päivitä "Lukitse" ja "Avaa" -tekstit
+    päivitäLukitseJaAvaaTekstit();
 }
 
 let kuvat;
@@ -86,10 +90,10 @@ function tarkistaVoitto() {
         lukitutRullat = [false, false, false, false];
         päivitäRullienTila();
 
-        // Aseta 1 sekunnin viive rullan kuvien vaihtoon
+        /*// Aseta 1 sekunnin viive rullan kuvien vaihtoon
         setTimeout(() => {
             pyoraytaRullat();
-        }, 1000);
+        }, 1000);*/
     } else {
         näytäVoittoIlmoitus(`Et voittanut tällä kertaa!`);
     }
@@ -98,29 +102,40 @@ function tarkistaVoitto() {
 function päivitäRullienTila() {
     rullaElementit.forEach((element, indeksi) => {
         element.style.backgroundColor = lukitutRullat[indeksi] ? 'lightgray' : 'white';
+        console.log(`Rullan ${indeksi} tila päivitetty: ${lukitutRullat[indeksi] ? 'Lukittu' : 'Ei lukittu'}`);
+
     });
 }
 
 function nollaaRullienLukitukset() {
     lukitutRullat = [false, false, false, false];
+    console.log("tila ennen", lukitutRullat)
     päivitäRullienTila();
+    console.log("Rullien lukitukset nollattu:", lukitutRullat);
+
 }
 pelaaButton.addEventListener('click', () => {
-    if (valittuPanos === 0) {
+    /*if (valittuPanos === 0) {
         alert('Valitse panos ennen pelaamista.');
         return;
     } /*toimiiko tämä kohta???*/
-    
+    console.log("Pelaa-paniketta painettiin", lukitutRullat);
+
     if (valittuPanos > saldo) {
         alert('Panos ei voi olla suurempi kuin saldo.');
         return;
     }
+
     saldo -= valittuPanos;
     saldoElementti.textContent = saldo;
 
     // Pyöritä rullat
     pyoraytaRullat();
-    voitto = tarkistaVoitto();
+    console.log("pyöritä rullat", pyoraytaRullat)
+    const voitto = tarkistaVoitto();
+    console.log("Pelaa-painikkeen käsittely päättyi", lukitutRullat);
+    nollaaRullienLukitukset()
+    päivitäLukitseJaAvaaTekstit()
 
 });
 
@@ -128,14 +143,27 @@ const lukitsePainikkeet = document.querySelectorAll('.lukitse');
 
 lukitsePainikkeet.forEach((painike, indeksi) => {
     painike.addEventListener('click', () => {
+        if (!lukitutRullat[indeksi] && lukitutRullat.filter(lukittu => lukittu).length >= 3) {
+            alert('Voit lukita enintään 3 rullaa.');
+            return;
+        }
+
         lukitutRullat[indeksi] = !lukitutRullat[indeksi];
         rullaElementit[indeksi].style.backgroundColor = lukitutRullat[indeksi] ? 'lightgray' : 'white'; 
         // Päivitä "Lukitse" ja "Avaa" -tekstit
-        const tilaElement = painike.querySelector('.tila');
-        if (lukitutRullat[indeksi]) {
-            tilaElement.textContent = "Avaa";
-        } else {
-            tilaElement.textContent = "Lukitse";
+
+        if (!voitto) {
+            lukitutRullat[indeksi] = !lukitutRullat[indeksi];
+            rullaElementit[indeksi].style.backgroundColor = lukitutRullat[indeksi] ? 'lightgray' : 'white'; 
+            // Päivitä "Lukitse" ja "Avaa" -tekstit
+            const tilaElement = painike.querySelector('.tila');
+            if (lukitutRullat[indeksi]) {
+                tilaElement.textContent = "Avaa";
+                console.log("avaa", lukitutRullat)
+            } else {
+                tilaElement.textContent = "Lukitse";
+                console.log("lukitse", lukitutRullat)
+            }
         }
     });
 });
@@ -160,3 +188,16 @@ function asetaAlkukuvat() {
 
 // Kutsu asetaAlkukuvat() pelin alussa
 asetaAlkukuvat();
+
+function päivitäLukitseJaAvaaTekstit() {
+    lukitsePainikkeet.forEach((painike, indeksi) => {
+        const tilaElement = painike.querySelector('.tila');
+        if (lukitutRullat[indeksi]) {
+            tilaElement.textContent = "Avaa";
+            console.log("avaa", lukitutRullat);
+        } else {
+            tilaElement.textContent = "Lukitse";
+            console.log("lukitse", lukitutRullat);
+        }
+    });
+}
